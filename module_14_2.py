6e9eace2691d75946ebe99c1c8ab51687be14c69
Romashka-Al <alinashkunova@yandex.ru>
2024-12-07 00:11:29 +0400

    Add files via upload

---
+++ module_14_2.py
@@ -0,0 +1,43 @@
+import sqlite3
+
+connection = sqlite3.connect('not_tg.db')
+cursor = connection.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+    id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    age INTEGER,
+    balance INTEGER NOT NULL
+    )
+''')
+
+#for i in range(1, 11):
+#    cursor.execute('INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)', (f'User{i}', f'ex{i}@gmail.com', i*10, 1000))
+
+for i in range(1, 11, 2):
+    cursor.execute('UPDATE Users SET balance = ? WHERE id= ?', (500, i))
+
+#for i in range(1, 11, 3):
+#    cursor.execute('DELETE FROM Users WHERE id = ?', (i,))
+
+cursor.execute('SELECT username, email, age, balance FROM Users WHERE age != ?', (60,))
+users = cursor.fetchall()
+#for user in users:
+#    print(f"Имя: {user[0]} | Почта: {user[1]} | Возраст: {user[2]} | Баланс: {user[3]}")
+
+cursor.execute('DELETE FROM Users WHERE id = ?', (6,))
+
+cursor.execute('SELECT COUNT(*) FROM Users')
+count_ = cursor.fetchone()[0]
+
+cursor.execute('SELECT SUM(balance) FROM Users')
+sum_balance = cursor.fetchone()[0]
+
+cursor.execute('SELECT AVG(balance) FROM Users')
+avg_balance = cursor.fetchone()[0]
+print(avg_balance) # или print(sum_balance / count_)
+
+connection.commit()
+connection.close()
